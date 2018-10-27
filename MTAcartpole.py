b36d11d5fc190c4f244bda89178f9c3b5fa4411c
kazimsanlav <kazimsanlav@gmail.com>
2018-10-27 11:24:40 +0300

    added swimmer class

--- MTAcartpole.py
+++ MTAcartpole.py
@@ -1,6 +1,14 @@
 import gym
-env = gym.make('Hopper-v2')
-env.reset()
-for _ in range(10000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+env = gym.make('CartPole-v0')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+
