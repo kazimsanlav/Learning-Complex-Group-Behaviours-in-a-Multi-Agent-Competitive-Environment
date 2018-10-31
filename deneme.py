3c3f7f97b799c5a6285a1de9de2fd82bf37820e4
kazimsanlav <kazimsanlav@gmail.com>
2018-10-31 19:36:13 +0300

    randomized step

--- deneme.py
+++ deneme.py
@@ -1,21 +1,46 @@
 import make_env_
-import multiagent
-import random
+
 # creates an multiagent environment which has reset, render, step
 env = make_env_.make_env('simple_tag')
+# create interactive policies for each agent
+# policies = [InteractivePolicy(env,i) for i in range(env.n)]
+
 for i_episode in range(15):
     observation = env.reset()
     for t in range(100):
         env.render()
-        # print(len(env.action_space))
-        # act_space = [[0, act_space.n - 1] for act_space in range(4)]
+        
 
-        action_n = [[0, random.random(), random.random(), random.random(), random.random()],
-                    [0, random.random(), random.random(), random.random(), random.random()],
-                    [0, random.random(), random.random(), random.random(), random.random()],
-                    [0, random.random(), random.random(), random.random(), random.random()]]
-        observation, reward, done, info = env.step(action_n)
+        # my_action0 = [ 
+        #     0,
+        #     env.action_space[0].sample(),
+        #     env.action_space[1].sample(),
+        #     env.action_space[2].sample(),
+        #     env.action_space[3].sample()
+        #     ]
+
+        def sample_actions():
+            action = [ 
+            0,
+            env.action_space[0].sample(),
+            env.action_space[1].sample(),
+            env.action_space[2].sample(),
+            env.action_space[3].sample()
+            ]
+            return action
+
+        my_action = [ sample_actions(), sample_actions(), sample_actions(), sample_actions() ]
+
+        
+        # my_action = [ my_action0, my_action0, my_action0, my_action0 ]
+
+        # print(my_action)
+        
+        observation, reward, done, info = env.step(my_action)
+
+
+
+
 env.close()
 
-# import random
-# print(random.sample([1,2,4,1,5,4],4))
+
